aa0d24e511173e1b47af08c18228f96066c23a08
songyw0517 <songyw0517@naver.com>
2020-03-02 13:09:48 +0900

    d

--- practice.js
+++ practice.js
@@ -1,16 +1,10 @@
-var num =1;
-var t = "<table border='1'>";
-for(var i=1; i<=5; i++){
-	t+="<tr>";
-
-	for(var k=1; k<=5; k++)
-	{
-		t += "<td>" + num + "</td>";
-		num++;
-	}
-	t+="</tr>";
+var cars = ["Saab", "Volvo", "BMW"];
+window.alert(cars[0][0]);
+document.write("The URL port number of the current page is: " + window.location.port);
+/*function alter_date(){
+	window.alert(Date());
+	document.getElementById("demo").innerHTML = Date();
 }
-
-t+="</table>";
-console.log(t);
-document.write(t);
+function alert_hover(){
+	window.alert("What the Fuck!");
+}*/
